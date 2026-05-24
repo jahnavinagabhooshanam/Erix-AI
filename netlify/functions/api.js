4d3aa9bcebcd3aabf6802e1eb29e75e9d3cd1e09
jahnavinagabhooshanam <jahnavinagabhooshanam@gmail.com>
2026-05-24 21:36:21 +0530

    Update project files

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
 import serverless from 'serverless-http';
 import app from '../../server/src/index.js';
 
-export const handler = serverless(app);
+export const handler = serverless(app, { basePath: '/.netlify/functions/api' });
